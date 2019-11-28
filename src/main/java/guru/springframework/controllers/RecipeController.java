95d42595a3807f2980d3aae6cb66b568f528eadf
ber.hanan <ber.hanan@gmail.com>
2019-11-28 18:45:41 +0200

    adding WebFlux Exception Handling

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 //import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -84,20 +85,16 @@ public class RecipeController {
         recipeService.deleteById(id);
         return "redirect:/";
     }
-/*
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
 
-        modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }*/
+        return "404Error";
+    }
 
 }
